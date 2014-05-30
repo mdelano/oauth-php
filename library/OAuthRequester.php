597a7857df73be3817959d469971a6e1d30d311a
Tori Holmes-Kirk <tori.kirk@gmail.com>
2014-05-30 13:28:37 -0400

    updated raw curl headers to correct content type

--- library/OAuthRequester.php
+++ library/OAuthRequester.php
@@ -358,7 +358,8 @@ class OAuthRequester extends OAuthRequestSigner
 			// Make sure that the content type of the request is ok
 			if (!$has_content_type)
 			{
-				$header[]         = 'Content-Type: application/octet-stream';
+				$header[] = 'Content-Type: application/json; charset=utf-8';
+				$header[] = 'Accept:application/json';
 				$has_content_type = true;
 			}
 			
